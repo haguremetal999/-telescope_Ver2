b669e423c9f09f9d39ae2ca8faafac2faddd243d
superkuppa99@gmail.com <superkuppa99@DESKTOP-MMAIGCJ.localdomain>
2020-09-19 17:48:58 +0900

    add new file

---
+++ build/CMakeFiles/exampleB4a.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/exampleB4a.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/exampleB4a.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/ActionInitialization.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/ActionInitialization.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/ChargeShare.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/ChargeShare.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/DetectorConstruction.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/DetectorConstruction.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/EventAction.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/EventAction.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/PhysicsList.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/PhysicsList.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/PrimaryGeneratorAction.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/RunAction.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/RunAction.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/SteppingAction.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/SteppingAction.cc.o"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/src/apixel.cc" "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/build/CMakeFiles/exampleB4a.dir/src/apixel.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_OPENGL_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/applications/geant4/geant4.10.06.p02-install/include/Geant4"
+  "/home/superkuppa99/workspace/G4WORK/telescope-work/PixelSize/t300/8um128px/source/include"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "/usr/share/qt4/mkspecs/default"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/exampleB4a.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleB4a.dir/exampleB4a.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/ChargeShare.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/EventAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/RunAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/exampleB4a.dir/src/apixel.cc.o"
+  "exampleB4a.pdb"
+  "exampleB4a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleB4a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
